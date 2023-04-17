c622c79e8b2bb8aafc0d3beec1f7e0e1c9286b95
chaudhuree <chaudhuree00@gmail.com>
2023-04-18 04:18:04 +0600

    2- create user service

---
+++ src/services/user/UserCreateService.js
@@ -0,0 +1,13 @@
+// Description: This file is used to create a new user
+// here Request is the req in the controller function and Datamodel is mongodb model
+const UserCreateService= async (Request,DataModel) => {
+    try{
+        let PostBody=Request.body;
+        let data = await DataModel.create(PostBody)
+        return {status: "success", data: data}
+    }
+    catch (error) {
+        return {status: "fail", data: error.toString()}
+    }
+}
+module.exports=UserCreateService
