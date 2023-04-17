62a3344d44f14f6b679d2674b36815af03d31204
chaudhuree <chaudhuree00@gmail.com>
2023-04-18 04:23:46 +0600

    added extra comment for easy understanding

--- src/services/user/UserDetailsService.js
+++ src/services/user/UserDetailsService.js
@@ -1,8 +1,10 @@
 const UserDetailsService= async (Request,DataModel) => {
     try {
         let data = await DataModel.aggregate([{$match: {email:Request.headers['email']}}])
+        // here data is an array of objects
         //or
         // let data = await DataModel.findOne({email:Request.headers['email']})
+        // here data is an object
         return  {status: "success", data: data}
     } catch (error) {
         return {status: "fail", data: error.toString()}
