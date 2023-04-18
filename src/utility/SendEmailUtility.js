b9107020378615e6c734b5588ae3dbc3a1a6f06b
chaudhuree <chaudhuree00@gmail.com>
2023-04-18 12:45:16 +0600

    add comment in sendEmailUtility.js

--- src/utility/SendEmailUtility.js
+++ src/utility/SendEmailUtility.js
@@ -3,21 +3,21 @@ var nodemailer = require('nodemailer');
 const SendEmailUtility = async (EmailTo, EmailText, EmailSubject) => {
 
     let transporter = nodemailer.createTransport({
-        host: 'mail.teamrabbil.com',
-        port: 25,
-        secure: false, // true for 465, false for other ports
+        host: 'mail.teamrabbil.com', //can create from gmail or yahoo
+        port: 25, // this port is provided by smtp server
+        secure: false, // as it is localhost so it is not secure and for 465 port it is secure
         auth: {
-            user: "info@teamrabbil.com",
-            pass: '~sR4[bhaC[Qs'
+            user: "info@teamrabbil.com", // email id
+            pass: '~sR4[bhaC[Qs' // password
         }, tls: {
-            rejectUnauthorized: false
+            rejectUnauthorized: false // this is for localhost
         },
     });
 
     let mailOptions = {
-        from: 'Inventory <info@teamrabbil.com>', // sender address
+        from: 'Inventory <info@teamrabbil.com>', // email mask. that is showed in email
         to: EmailTo, // list of receivers,i.e: "ch@mail.com,sk@mail.com"
-        subject: EmailSubject, // Subject line
+        subject: EmailSubject, // Subject line,it is shown inside email
         text: EmailText // plain text body
     };
 
