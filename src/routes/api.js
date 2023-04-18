0b61f15a3278df5b4ba9bd6fb9f7df3413ac9a9f
chaudhuree <chaudhuree00@gmail.com>
2023-04-18 13:54:04 +0600

    14 - user model and controller related all routing added.

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,4 +1,20 @@
 const express = require('express');
+// middleware for verify token and authorization
+const AuthVerifyMiddleware=require("../middlewares/AuthVerifyMiddleware");
+// user controller for CRUD and reset password
+const UsersController=require("../controllers/Users/UsersController");
+
 const router = express.Router();
+
+
+// User Profile
+router.post("/Registration",UsersController.Registration);
+router.post("/Login",UsersController.Login);
+router.post("/ProfileUpdate",AuthVerifyMiddleware,UsersController.ProfileUpdate);
+router.get("/ProfileDetails",AuthVerifyMiddleware,UsersController.ProfileDetails);
+router.get("/RecoverVerifyEmail/:email",UsersController.RecoverVerifyEmail);
+router.get("/RecoverVerifyOTP/:email/:otp",UsersController.RecoverVerifyOTP);
+router.post("/RecoverResetPass",UsersController.RecoverResetPass);
+
 
 module.exports = router;
