879ab662eef5ad4992fda9cfddcadf24d7dd8316
chaudhuree <chaudhuree00@gmail.com>
2023-04-18 13:48:57 +0600

    12 - reset password controller

--- src/controllers/Users/UsersController.js
+++ src/controllers/Users/UsersController.js
@@ -4,6 +4,9 @@ const UserCreateService = require("../../services/user/UserCreateService");
 const UserLoginService = require("../../services/user/UserLoginService");
 const UserUpdateService = require("../../services/user/UserUpdateService");
 const UserDetailsService = require("../../services/user/UserDetailsService");
+const UserResetPassService = require("../../services/user/UserResetPassService");
+const UserVerifyOtpService = require("../../services/user/UserVerifyOtpService");
+const UserVerifyEmailService = require("../../services/user/UserVerifyEmailService");
 
 
 
@@ -26,6 +29,23 @@ exports.ProfileDetails=async (req, res) => {
     let Result=await UserDetailsService(req,DataModel)
     res.status(200).json(Result)
 }
+
+exports.RecoverVerifyEmail=async (req,res)=>{
+    let Result=await UserVerifyEmailService(req,DataModel)
+    res.status(200).json(Result)
+}
+
+
+exports.RecoverVerifyOTP=async (req,res)=>{
+    let Result=await UserVerifyOtpService(req,OTPSModel)
+    res.status(200).json(Result)
+}
+
+exports.RecoverResetPass=async (req,res)=>{
+    let Result=await UserResetPassService(req,DataModel)
+    res.status(200).json(Result)
+}
+
 
 
 
