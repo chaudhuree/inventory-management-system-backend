1a73f6b74158cc4dfcf76c3eb8ba1eb81d744464
chaudhuree <chaudhuree00@gmail.com>
2023-04-20 12:02:32 +0600

    20 - brands controller is created here i have done creating ,updating , listing(searching and pagination) finally dropdown where i get the name and id form the Brands model

---
+++ src/controllers/Brands/BrandsController.js
@@ -0,0 +1,35 @@
+const DataModel = require("../../models/Brands/BrandsModel");
+const CreateService = require("../../services/common/CreateService");
+const UpdateService = require("../../services/common/UpdateService");
+const ListService = require("../../services/common/ListService");
+const DropDownService = require("../../services/common/DropDownService");
+
+
+exports.CreateBrand = async (req, res) => {
+    let Result = await CreateService(req, DataModel)
+    res.status(200).json(Result)
+}
+
+
+exports.UpdateBrand = async (req, res) => {
+    let Result = await UpdateService(req, DataModel)
+    res.status(200).json(Result)
+}
+
+
+exports.BrandList = async (req, res) => {
+
+    let SearchRgx = { "$regex": req.params.searchKeyword, "$options": "i" } // i for case insensitive
+    let SearchArray = [{ Name: SearchRgx }]
+
+    let Result = await ListService(req, DataModel, SearchArray)
+    res.status(200).json(Result)
+}
+
+
+exports.BrandDropDown = async (req, res) => {
+    let Result = await DropDownService(req, DataModel, { _id: 1, Name: 1 }) // 1 for include
+    // so here we only want to include _id and Name
+    res.status(200).json(Result)
+}
+
