e1fac3284c07e796b959f858b176c0a4210ab81c
chaudhuree <chaudhuree00@gmail.com>
2023-04-21 11:18:28 +0600

    25 - customer model added

---
+++ src/models/Customers/CustomersModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const DataSchema = mongoose.Schema(
+  {
+    UserEmail: { type: String },
+    CustomerName: { type: String },
+    Phone: { type: String, unique: true },
+    Email: { type: String },
+    Address: { type: String },
+    CreatedDate: { type: Date, default: Date.now() },
+  },
+  { versionKey: false }
+);
+const CustomersModel = mongoose.model("customers", DataSchema);
+module.exports = CustomersModel;
