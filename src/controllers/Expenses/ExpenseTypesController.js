6a1b63dc0eddbb96aa10014cd467820018e72685
chaudhuree <chaudhuree00@gmail.com>
2023-04-21 11:44:28 +0600

    32 - expense type controller added

---
+++ src/controllers/Expenses/ExpenseTypesController.js
@@ -0,0 +1,28 @@
+const DataModel = require("../../models/Expenses/ExpenseTypesModel");
+const CreateService = require("../../services/common/CreateService");
+const UpdateService = require("../../services/common/UpdateService");
+const ListService = require("../../services/common/ListService");
+const DropDownService = require("../../services/common/DropDownService");
+
+
+exports.CreateExpenseTypes = async (req, res) => {
+    let Result = await CreateService(req, DataModel)
+    res.status(200).json(Result)
+}
+
+exports.UpdateExpenseTypes = async (req, res) => {
+    let Result = await UpdateService(req, DataModel)
+    res.status(200).json(Result)
+}
+
+exports.ExpenseTypesList = async (req, res) => {
+    let SearchRgx = { "$regex": req.params.searchKeyword, "$options": "i" }
+    let SearchArray = [{ Name: SearchRgx }]
+    let Result = await ListService(req, DataModel, SearchArray)
+    res.status(200).json(Result)
+}
+
+exports.ExpenseTypesDropDown = async (req, res) => {
+    let Result = await DropDownService(req, DataModel, { _id: 1, Name: 1 })
+    res.status(200).json(Result)
+}
