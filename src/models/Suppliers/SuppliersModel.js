f94c19822ffd5ebeebf3829e25f8ac24c7236dfe
chaudhuree <chaudhuree00@gmail.com>
2023-04-21 11:22:43 +0600

    28 - suppliers model added

---
+++ src/models/Suppliers/SuppliersModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const DataSchema = mongoose.Schema(
+  {
+    UserEmail: { type: String },
+    Name: { type: String },
+    Address: { type: String },
+    Phone: { type: String, unique: true },
+    Email: { type: String },
+    CreatedDate: { type: Date, default: Date.now() },
+  },
+  { versionKey: false }
+);
+const SuppliersModel = mongoose.model("suppliers", DataSchema);
+module.exports = SuppliersModel;
