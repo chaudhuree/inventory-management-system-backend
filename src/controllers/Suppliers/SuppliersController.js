5865e4fabd2f480f6d716103398a3100a18cdd88
chaudhuree <chaudhuree00@gmail.com>
2023-04-21 11:24:45 +0600

    29 - suppliers controller added

---
+++ src/controllers/Suppliers/SuppliersController.js
@@ -0,0 +1,55 @@
+const DataModel = require("../../models/Suppliers/SuppliersModel");
+const CreateService = require("../../services/common/CreateService");
+const UpdateService = require("../../services/common/UpdateService");
+const ListService = require("../../services/common/ListService");
+const DropDownService = require("../../services/common/DropDownService");
+
+exports.CreateSuppliers = async (req, res) => {
+    let Result = await CreateService(req, DataModel);
+    res.status(200).json(Result);
+};
+
+exports.UpdateSuppliers = async (req, res) => {
+    let Result = await UpdateService(req, DataModel);
+    res.status(200).json(Result);
+};
+
+// in this function we have multiple search fields
+exports.SuppliersList = async (req, res) => {
+    let SearchRgx = { $regex: req.params.searchKeyword, $options: "i" };
+    let SearchArray = [
+        { Name: SearchRgx },
+        { Phone: SearchRgx },
+        { Email: SearchRgx },
+        { Address: SearchRgx },
+    ];
+    let Result = await ListService(req, DataModel, SearchArray);
+    res.status(200).json(Result);
+};
+
+exports.SuppliersDropDown = async (req, res) => {
+    let Result = await DropDownService(req, DataModel, { _id: 1, Name: 1 });
+    res.status(200).json(Result);
+};
+
+exports.SuppliersDetailsByID = async (req, res) => {
+    let Result = await DetailsByIDService(req, DataModel);
+    res.status(200).json(Result);
+};
+
+exports.DeleteSupplier = async (req, res) => {
+    let DeleteID = req.params.id;
+    const ObjectId = mongoose.Types.ObjectId;
+    let CheckAssociate = await CheckAssociateService(
+        { SupplierID: ObjectId(DeleteID) },
+        PurchasesModel
+    );
+    if (CheckAssociate) {
+        res
+            .status(200)
+            .json({ status: "associate", data: "Associate with Purchases" });
+    } else {
+        let Result = await DeleteService(req, DataModel);
+        res.status(200).json(Result);
+    }
+};
