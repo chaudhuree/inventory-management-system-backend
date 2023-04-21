1bc36f29e0ecb05cb1a3e72e37186de137f42af6
chaudhuree <chaudhuree00@gmail.com>
2023-04-21 11:15:07 +0600

    23 - categories controller added

---
+++ src/controllers/Categories/CategoriesController.js
@@ -0,0 +1,33 @@
+const DataModel = require("../../models/Categories/CategoriesModel");
+const CreateService = require("../../services/common/CreateService");
+const UpdateService = require("../../services/common/UpdateService");
+const ListService = require("../../services/common/ListService");
+const DropDownService = require("../../services/common/DropDownService");
+
+
+exports.CreateCategories = async (req, res) => {
+    let Result = await CreateService(req, DataModel)
+    res.status(200).json(Result)
+}
+
+exports.UpdateCategories = async (req, res) => {
+    let Result = await UpdateService(req, DataModel)
+    res.status(200).json(Result)
+}
+
+
+exports.CategoriesList = async (req, res) => {
+    let SearchRgx = { "$regex": req.params.searchKeyword, "$options": "i" }
+    let SearchArray = [{ Name: SearchRgx }]
+    let Result = await ListService(req, DataModel, SearchArray)
+    res.status(200).json(Result)
+}
+
+
+exports.CategoriesDropDown = async (req, res) => {
+    let Result = await DropDownService(req, DataModel, { _id: 1, Name: 1 })
+    res.status(200).json(Result)
+}
+
+
+
