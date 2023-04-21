aa25c471210f58be273529ffcb4e43a6134c7ab0
chaudhuree <chaudhuree00@gmail.com>
2023-04-21 11:43:06 +0600

    31 - expense type model added

---
+++ src/models/Expenses/ExpenseTypesModel.js
@@ -0,0 +1,12 @@
+// name should be unique
+const mongoose = require("mongoose");
+const DataSchema = mongoose.Schema(
+    {
+        UserEmail: { type: String },
+        Name: { type: String, unique: true },
+        CreatedDate: { type: Date, default: Date.now() },
+    },
+    { versionKey: false }
+);
+const ExpenseTypesModel = mongoose.model("expensetypes", DataSchema);
+module.exports = ExpenseTypesModel;
