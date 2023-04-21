5cb0070e58e9a2fd28623e4f0a5261ada682c4ce
chaudhuree <chaudhuree00@gmail.com>
2023-04-21 11:19:53 +0600

    26 - customers controller added

---
+++ src/controllers/Customers/CustomersController.js
@@ -0,0 +1,33 @@
+const DataModel = require("../../models/Customers/CustomersModel");
+const CreateService = require("../../services/common/CreateService");
+const UpdateService = require("../../services/common/UpdateService");
+const ListService = require("../../services/common/ListService");
+const DropDownService = require("../../services/common/DropDownService");
+
+
+exports.CreateCustomers = async (req, res) => {
+    let Result = await CreateService(req, DataModel)
+    res.status(200).json(Result)
+}
+
+exports.UpdateCustomers = async (req, res) => {
+    let Result = await UpdateService(req, DataModel)
+    res.status(200).json(Result)
+}
+
+// in brand and category we have only one search field but in customer we have multiple search fields
+exports.CustomersList = async (req, res) => {
+    let SearchRgx = { "$regex": req.params.searchKeyword, "$options": "i" }
+    let SearchArray = [{ CustomerName: SearchRgx }, { Phone: SearchRgx }, { Email: SearchRgx }, { Address: SearchRgx }]
+    let Result = await ListService(req, DataModel, SearchArray)
+    res.status(200).json(Result)
+}
+
+exports.CustomersDropDown = async (req, res) => {
+    let Result = await DropDownService(req, DataModel, { _id: 1, CustomerName: 1 })
+    res.status(200).json(Result)
+}
+
+
+
+
