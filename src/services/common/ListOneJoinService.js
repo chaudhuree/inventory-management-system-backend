ae09ae06561fc7d9373a082f4224df9afc37d588
chaudhuree <chaudhuree00@gmail.com>
2023-04-22 00:14:44 +0600

    38 - ListOneJoinServices for listing data that has mongoose.Schema.Types.ObjectId or joining two models

---
+++ src/services/common/ListOneJoinService.js
@@ -0,0 +1,48 @@
+const ListOneJoinService = async (Request, DataModel, SearchArray, JoinStage) => {
+    try {
+        let pageNo = Number(Request.params.pageNo);
+        let perPage = Number(Request.params.perPage);
+        let searchValue = Request.params.searchKeyword;
+        let UserEmail = Request.headers['email'];
+        let skipRow = (pageNo - 1) * perPage;
+
+        let data;
+
+        if (searchValue !== "0") {
+            data = await DataModel.aggregate([
+                { $match: { UserEmail: UserEmail } },
+                JoinStage,
+                { $match: { $or: SearchArray } },
+                {
+                    $facet: {
+                        Total: [{ $count: "count" }],
+                        Rows: [{ $skip: skipRow }, { $limit: perPage }]
+                    }
+                }
+            ])
+
+        }
+        else {
+            data = await DataModel.aggregate([
+                { $match: { UserEmail: UserEmail } },
+                JoinStage,
+                {
+                    $facet: {
+                        Total: [{ $count: "count" }],
+                        Rows: [{ $skip: skipRow }, { $limit: perPage }]
+                    }
+                }
+            ])
+            // or
+            //dataList=DataModel.find({UserEmail:UserEmail}).populate("TypeID").skip(skipRow).limit(perPage)
+            // or
+            //dataCount=DataModel.countDocuments({UserEmail:UserEmail})
+            //data:{ Total: dataCount, Rows: dataList }
+        }
+        return { status: "success", data: data }
+    }
+    catch (error) {
+        return { status: "fail", data: error }
+    }
+}
+module.exports = ListOneJoinService
