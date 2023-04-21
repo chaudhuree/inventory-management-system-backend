b974fbedc086c847ba4fd1aab845c60ae86e6f84
chaudhuree <chaudhuree00@gmail.com>
2023-04-21 11:14:01 +0600

    22 - categories model added

---
+++ src/models/Categories/CategoriesModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const DataSchema = mongoose.Schema(
+  {
+    UserEmail: { type: String },
+    Name: { type: String, unique: true },
+    CreatedDate: { type: Date, default: Date.now() },
+  },
+  { versionKey: false }
+);
+const CategoriesModel = mongoose.model("categories", DataSchema);
+module.exports = CategoriesModel;
