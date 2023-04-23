9c9683efba9e9ba6eca0f655828022dc4a35a9b8
chaudhuree <chaudhuree00@gmail.com>
2023-04-23 15:37:54 +0600

    39 - expensesController expenseslist functionality

--- src/controllers/Expenses/ExpensesController.js
+++ src/controllers/Expenses/ExpensesController.js
@@ -14,3 +14,19 @@ exports.UpdateExpenses = async (req, res) => {
 }
 
 
+exports.ExpensesList = async (req, res) => {
+    let SearchRgx = { "$regex": req.params.searchKeyword, "$options": "i" }
+    let SearchArray = [{ Note: SearchRgx }, { 'Type.Name': SearchRgx }]
+    // in SearchArray we are not only searching by Note but also in Type.Name.
+    // so we can search from expensetypes collection also.
+    // as in the JoinStage it is named as Type so in SearchArray we are passing it
+    // as Type.Name
+
+    let JoinStage = { $lookup: { from: "expensetypes", localField: "TypeID", foreignField: "_id", as: "Type" } }
+
+
+    let Result = await ListOneJoinService(req, DataModel, SearchArray, JoinStage);
+    res.status(200).json(Result)
+}
+
+
