62868d4f864288a805f9e5ad7bddf9c663748cab
chaudhuree <chaudhuree00@gmail.com>
2023-04-27 04:10:52 +0600

    52 - CreateParentChildService with transaction rolled back system. for these i need to transfer the whole data from local to atlas. otherwise it throws error, " Transaction numbers are only allowed on a replica set member or mong"

--- src/controllers/Purchases/PurchasesController.js
+++ src/controllers/Purchases/PurchasesController.js
@@ -1,10 +1,13 @@
 const ParentModel = require("../../models/Purchases/PurchasesModel"); // ParentModel that mean for purchase the model is PurchasesModel. but for better understanding it is imported as ParentModel
 const ChildsModel = require("../../models/Purchases/PurchaseProductsModel");
 // like ParentModel, PurchaseProductsModel is imported as ChildsModel
-const CreateParentChildsService = require("../../services/common/CreateParentChildsService");
+
+
+// const CreateParentChildsService = require("../../services/common/CreateParentChildsService");
+const CreateParentChildsServiceWithTransaction = require("../../services/common/CreateParentChildsServiceWithTransaction");
 
 
 exports.CreatePurchases = async (req, res) => {
-    let Result = await CreateParentChildsService(req, ParentModel, ChildsModel, 'PurchaseID');
+    let Result = await CreateParentChildsServiceWithTransaction(req, ParentModel, ChildsModel, 'PurchaseID');
     res.status(200).json(Result)
 }
